7127e5ca48b5c1c6f966731c21223a8bb85ea881
Emir Krainovic <emir.krainovic@serapion.net>
2021-12-28 16:29:14 +0100

    Added login compontent.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/ordering/ordering.module').then((m) => m.OrderingModule),
   },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginModule),
+  },
 ];
 
 @NgModule({
