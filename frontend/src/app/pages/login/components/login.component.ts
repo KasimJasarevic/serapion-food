bf91ceabe7ce9c2f4fa31232c3537620a0f4fdb4
Kasim Jasarevic <kasim.jasarevic@serapion.net>
2022-02-03 12:57:09 +0100

    Changed: Google login URL production and development

--- frontend/src/app/pages/login/components/login.component.ts
+++ frontend/src/app/pages/login/components/login.component.ts
@@ -5,7 +5,7 @@ import { LocalStorageTypes } from 'src/app/core/enums/local-storage-types';
 import { IUser } from 'src/app/core/models/user.model';
 import { UserService } from 'src/app/core/services/user.service';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../environments/environment';
+import { environment } from '@environments/environment';
 import { SubSink } from '@core/helpers/sub-sink';
 import { ThemeService } from 'src/app/shared/services/theme.service';
 
@@ -67,8 +67,7 @@ export class LoginComponent implements OnInit, OnDestroy {
   ngOnInit(): void {}
 
   googleSignIn() {
-    window.location.href =
-      'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=895104585556-t3i4u6236nkjhjf4cnr3ua81l9dq7kt0.apps.googleusercontent.com';
+    window.location.href = environment.google_login_url;
   }
 
   changeTheme() {
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   notificationId: '184f560a-3c28-4296-8f6e-854cfee824b0',
   signal_one_api_key: 'MDU2N2Y5ODAtZTVjZS00YzI1LTg0YjAtMWM4ZTdkNjI0YzU0',
   api_url: 'api',
-  ws_url: 'http://localhost:3000'
+  ws_url: 'http://localhost:3000',
+  google_login_url: 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=895104585556-t3i4u6236nkjhjf4cnr3ua81l9dq7kt0.apps.googleusercontent.com'
 };
