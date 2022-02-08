bac9ba49e6c34f5600e682b7a8d2dd9540e77fc3
Kasim Jasarevic <kasim.jasarevic@serapion.net>
2022-02-08 22:38:54 +0100

    Changed: WS URL in production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   notificationId: '184f560a-3c28-4296-8f6e-854cfee824b0',
   signal_one_api_key: 'MDU2N2Y5ODAtZTVjZS00YzI1LTg0YjAtMWM4ZTdkNjI0YzU0',
   api_url: 'api',
-  ws_url: 'http://192.168.0.96:3000'
+  ws_url: ''
 };
